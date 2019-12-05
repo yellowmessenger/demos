4fbb2c1bf69056d586fbabbe7081f4ea8562280b
saddle45 <36154681+saddle45@users.noreply.github.com>
2019-12-05 10:41:41 +0530

    Update script.js

--- mahesh/script.js
+++ mahesh/script.js
@@ -7,6 +7,17 @@ const liked = () => {
 	links.classList.remove('links');
 	filter.innerHTML = '<i class="fas fa-thumbs-up"></i>';
 	filter.classList.add("rated");
+	window.parent.postMessage(JSON.stringify({
+                        event_code: 'ym-client-event', data: JSON.stringify({
+                            event: {
+                                code: "Feedback",
+                                data: {
+                                    feedback: 'liked'
+                                }
+                            }
+                        })
+                    }), '*');
+	console.log('done')
 }
 
 const disliked = () => {
@@ -14,6 +25,16 @@ const disliked = () => {
 	links.classList.remove('links');
 	filter.innerHTML = '<i class="fas fa-thumbs-down"></i>';
 	filter.classList.add("rated");
+	 window.parent.postMessage(JSON.stringify({
+                        event_code: 'ym-client-event', data: JSON.stringify({
+                            event: {
+                                code: "Feedback",
+                                data: {
+                                    feedback: 'disliked'
+                                }
+                            }
+                        })
+                    }), '*');
 	
 }
 
