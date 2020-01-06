745c6d1117600eb974b6b7bb4d408e550e1441cd
priyankubh <51198910+priyankubh@users.noreply.github.com>
2020-01-06 16:20:44 +0530

    Update livechatscript.js

--- BYJUS/livechatscript.js
+++ BYJUS/livechatscript.js
@@ -22,9 +22,9 @@ var LC_API = LC_API || {};
     LC_API.on_before_load = function () {
         // don't hide the chat window only if visitor
         // is currently chatting with an agent
-        if (LC_API.visitor_engaged() === false && livechat_chat_started === false) {
+        //if (LC_API.visitor_engaged() === false && livechat_chat_started === false) {
             LC_API.hide_chat_window();
-        }
+        //}
     };
 
     LC_API.on_chat_started = function () {
