a51f8c17e24f2c4b5290e393dd16dec5cefe56db
priyankubh <51198910+priyankubh@users.noreply.github.com>
2020-01-09 16:08:21 +0530

    livechat hide

--- BYJUS/livechatscript.js
+++ BYJUS/livechatscript.js
@@ -1,15 +1,14 @@
 // <!-- Start of LiveChat (www.livechatinc.com) code -->
 var LC_API = LC_API || {};
 
-// LC_API.on_after_load = function() {
-// 	LC_API.hide_chat_window();
-// };
+LC_API.on_after_load = function() {
+	LC_API.hide_chat_window();
+};
 
 // LC_API.on_before_load = function() {
 // 	LC_API.hide_chat_window();
 // };
 
-//window.onload = function () {
     window.__lc = window.__lc || {};
     window.__lc.license = 1952541;
     //window.__lc.group = 23;
@@ -17,19 +16,19 @@ var LC_API = LC_API || {};
     lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(lc, s);
-    var livechat_chat_started = false;
+//     var livechat_chat_started = false;
 
-    LC_API.on_before_load = function () {
+    //LC_API.on_before_load = function () {
         // don't hide the chat window only if visitor
         // is currently chatting with an agent
         //if (LC_API.visitor_engaged() === false && livechat_chat_started === false) {
-            LC_API.hide_chat_window();
+            //LC_API.hide_chat_window();
         //}
-    };
+    //};
 
-    LC_API.on_chat_started = function () {
-        livechat_chat_started = true;
-    };
+//     LC_API.on_chat_started = function () {
+//         livechat_chat_started = true;
+//     };
 //};
 
 window.addEventListener("message", function (event) {
