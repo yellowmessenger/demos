20162671cd905191f9c776eb74ecd6a58c61ef7a
priyankubh <51198910+priyankubh@users.noreply.github.com>
2020-02-11 15:50:43 +0530

    added callback on_after_load in transfer to agent

--- BYJUS/livechatscript.js
+++ BYJUS/livechatscript.js
@@ -5,10 +5,6 @@ LC_API.on_after_load = function() {
 	LC_API.hide_chat_window();
 };
 
-// LC_API.on_before_load = function() {
-// 	LC_API.hide_chat_window();
-// };
-
     window.__lc = window.__lc || {};
     window.__lc.license = 1952541;
     //window.__lc.group = 23;
@@ -16,22 +12,8 @@ LC_API.on_after_load = function() {
     lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(lc, s);
-//     var livechat_chat_started = false;
 
-    //LC_API.on_before_load = function () {
-        // don't hide the chat window only if visitor
-        // is currently chatting with an agent
-        //if (LC_API.visitor_engaged() === false && livechat_chat_started === false) {
-            //LC_API.hide_chat_window();
-        //}
-    //};
-
-//     LC_API.on_chat_started = function () {
-//         livechat_chat_started = true;
-//     };
-//};
-
-window.addEventListener("message", function (event) {
+    window.addEventListener("message", function (event) {
     var ldata = {}
     try {
         event.data = JSON.parse(event.data);
@@ -41,12 +23,17 @@ window.addEventListener("message", function (event) {
         //do nothing
     }
     if (ldata.code === 'transfer_to_agent') {
+	    
         //window.YellowMessengerPlugin.removeChat();
-	window.YellowMessengerPlugin.hide();    
-        var custom_variables = [
-            { name: 'Chat History', value: ldata.data.chatHistory }
-        ];
-        LC_API.set_custom_variables(custom_variables);
-        LC_API.open_chat_window();
+	//window.YellowMessengerPlugin.hide();    
+        //var custom_variables = [
+        //    { name: 'Chat History', value: ldata.data.chatHistory }
+        //];
+        //LC_API.set_custom_variables(custom_variables);
+	    
+        LC_API.on_after_load = function () {
+          window.YellowMessengerPlugin.hide();
+          LC_API.open_chat_window();
+        };
     }
 }, false);
