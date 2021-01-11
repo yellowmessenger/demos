294f63fd018006a349f4bfcb23186a97364f5f40
priyankubh <51198910+priyankubh@users.noreply.github.com>
2021-01-11 23:06:43 +0530

    Update livechatinc_v1.js

--- BYJUS/livechatinc_v1.js
+++ BYJUS/livechatinc_v1.js
@@ -1,6 +1,12 @@
 // <!-- Start of LiveChat (www.livechatinc.com) code -->
     var LC_API = LC_API || {};
 
+	window.__lc = window.__lc || {};
+        window.__lc.license = 1952541;
+        var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+        lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+        var s = document.getElementsByTagName('script')[0];
+
     window.addEventListener("message", function (event) {
     var ldata = {}
     try {
@@ -11,13 +17,7 @@
         //do nothing
     }
     if (typeof(ldata) == "object" && ldata.hasOwnProperty("code") && ldata.code === "transfer_to_agent") {
-	        window.__lc = window.__lc || {};
-          window.__lc.license = 1952541;
-          var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-          lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-          var s = document.getElementsByTagName('script')[0];
           s.parentNode.insertBefore(lc, s);
           window.YellowMessengerPlugin.hide();
-          LC_API.open_chat_window();
     }
 }, false);
