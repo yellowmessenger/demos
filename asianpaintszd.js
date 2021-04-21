91e4d008b1a8655ed82a2c6698ce77376e51256a
priyankubh <51198910+priyankubh@users.noreply.github.com>
2021-04-21 10:14:35 +0530

    Create asianpaintszd.js

---
+++ asianpaintszd.js
@@ -0,0 +1,23 @@
+var zendesk = document.createElement('script');
+
+zendesk.setAttribute('src','https://static.zdassets.com/ekr/snippet.js?key=90d22fd8-78bf-4e93-9c7e-31659ad2cd94');
+
+document.head.appendChild(zendesk);
+
+window.addEventListener("message", function (event) {
+
+			var ldata = {}
+			try {
+				event.data = JSON.parse(event.data);
+				ldata = JSON.parse(event.data);
+				ldata = ldata.data
+
+			} catch (e) {
+				//do nothing
+			}
+
+			if (ldata.code === 'transfer_to_agent') {
+ 	          window.YellowMessengerPlugin.hide();
+				zE('webWidget', 'open');
+	}
+		}, false);
