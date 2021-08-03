6e6b8cf6a0fdd0eeb1d4440a9f123bc4a2869867
surender <say2surender@gmail.com>
2021-08-03 16:07:43 +0530

    customjs

---
+++ RE/custom.js
@@ -0,0 +1,5 @@
+var x = document.getElementsByClassName("title-parent");
+var element= document.createElement("img");   
+element.setAttribute("class", "icon1");
+element.style.maxWidth = "200px";
+element.style.margin = 0;
