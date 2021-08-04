7524b7f47fa305d141b1b0a93cd5cebdc96e29a3
surender <say2surender@gmail.com>
2021-08-04 19:20:30 +0530

    RE custom.js

--- RE/custom.js
+++ RE/custom.js
@@ -1,5 +1,16 @@
+var a = document.getElementsByClassName("title");
+  while(a.length > 0){
+        a[0].parentNode.removeChild(a[0]);
+}
+var b=document.getElementsByClassName("sub-title");
+while(b.length > 0){
+        b[0].parentNode.removeChild(b[0]);
+}
 var x = document.getElementsByClassName("title-parent");
 var element= document.createElement("img");   
 element.setAttribute("class", "icon1");
 element.style.maxWidth = "200px";
-element.style.margin = 0;
+element.style.margin = '-10px 0 0 -25px'
+element.style.padding = 0;
+element.setAttribute("src", "https://cdn.yellowmessenger.com/JrLTt9MlqGSU1627640891858.PNG");
+x[0].appendChild(element)
